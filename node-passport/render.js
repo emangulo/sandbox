79f9d26cbc4233302004b975e11fc07d601e50fe
Elias <emangulo1@gmail.com>
2024-06-10 13:47:28 -0700

    Add ejs functionality

--- node-passport/render.js
+++ node-passport/render.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
 app.set("view engine", "ejs");
+
+app.use("/public", express.static(path.join(__dirname, "public")));
 
 app.get("/login", (req, res) => {
   res.render("login");
 });
 
-app.get("/app", (req, res) => {
-  res.render("app");
+app.get("/profile", (req, res) => {
+  res.render("profile");
 });
 
 app.listen(3000, () => {
