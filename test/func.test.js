e7d6b0e4147d4256c77dff30c08dc473589a472e
Elias Angulo <emangulo1@gmail.com>
2024-07-14 09:58:22 -0700

    add test case

--- test/func.test.js
+++ test/func.test.js
@@ -3,8 +3,10 @@ import assert from "node:assert/strict";
 
 import { addNumbers } from "../func.js";
 
-describe("This is the test", () => {
-  test("Two numbers add up", () => {
-    assert(addNumbers(3, 4) == 3 + 4);
-  });
+test("This test should pass", () => {
+  assert(addNumbers(3, 4) == 3 + 4);
+});
+
+test("This test should fail", () => {
+  assert(false);
 });
