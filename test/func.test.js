89dfda2ffe685f079fe9ecfa971f0dff9c66c397
Elias Angulo <emangulo1@gmail.com>
2025-04-23 19:20:41 -0700

    clean test

--- test/func.test.js
+++ test/func.test.js
@@ -1,16 +1,13 @@
-import { describe, test } from "mocha";
 import assert from "node:assert/strict";
 
 import { addNumbers } from "../func.js";
 
 describe("func.test.js", () => {
-  describe("These are the test", () => {
-    test("This test should pass", () => {
-      assert(addNumbers(3, 4) == 3 + 4);
-    });
+  it("This test should pass", () => {
+    assert(addNumbers(3, 4) == 3 + 4);
+  });
 
-    test("This test should fail", () => {
-      assert(!false);
-    });
+  it("This test should fail", () => {
+    assert(!false);
   });
 });
